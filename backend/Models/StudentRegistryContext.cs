0a88a34cc067a46707137514e2a6aeff1fda7890
Csaba Szilágyi <csabi32105@gmail.com>
2024-11-07 18:57:50 +0100

    Initialize project

---
+++ backend/Models/StudentRegistryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Models;
+
+public class StudentRegistryContext : DbContext
+{
+    public StudentRegistryContext() { }
+
+    public StudentRegistryContext(DbContextOptions<StudentRegistryContext> options)
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
