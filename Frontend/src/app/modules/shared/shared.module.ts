a156b6900752bc631a2f0d83c6d8d5bcda0dbf03
Csaba Szilágyi <csabi32105@gmail.com>
2024-11-08 20:45:43 +0100

    Multi language

--- Frontend/src/app/modules/shared/shared.module.ts
+++ Frontend/src/app/modules/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   exports: [
     CommonModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class SharedModule { }
