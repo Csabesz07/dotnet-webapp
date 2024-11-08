94276a819b03f6ee4ad1db858960143abb33ede9
Csaba Szilágyi <csabi32105@gmail.com>
2024-11-08 10:28:05 +0100

    Configure DB Context

--- backend/Models/StudentRegistryContext.cs
+++ backend/Models/StudentRegistryContext.cs
@@ -1,19 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Models.StudentRegistry;
+using Microsoft.EntityFrameworkCore;
 
 namespace Core.Models;
 
 public class StudentRegistryContext : DbContext
 {
-    public StudentRegistryContext() { }
+    public StudentRegistryContext() 
+    {
+        Database.SetCommandTimeout(240);
+    }
 
     public StudentRegistryContext(DbContextOptions<StudentRegistryContext> options)
         : base(options)
     {
-        
+        Database.SetCommandTimeout(240);
     }
+
+    public virtual DbSet<Student> Students { get; set; } = null!;
+    public virtual DbSet<StudentGrade> StudentGrades { get; set; } = null!;
+    public virtual DbSet<Subject> Subjects { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        modelBuilder.ApplyConfiguration(new Student.Configuration());
+        modelBuilder.ApplyConfiguration(new StudentGrade.Configuration());
+        modelBuilder.ApplyConfiguration(new Subject.Configuration());
     }
 }
